7bcea6eea896bdb66a0f14232c01c396a27b1f27
wardac <wardac@miamioh.edu>
2014-10-09 13:17:31 -0400

    Revert "DB Test"
    This reverts commit e9912b1e42f810704721ffc3fa5e7e92b736b96f.

--- FRCApp/fc_tab.Designer.cs
+++ FRCApp/fc_tab.Designer.cs
@@ -30,21 +30,21 @@
         {
             this.fc_firstname_label = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.fc_email_textbox = new System.Windows.Forms.TextBox();
-            this.fc_phoneNumber2_textbox = new System.Windows.Forms.TextBox();
-            this.fc_phoneNumber1_textbox = new System.Windows.Forms.TextBox();
-            this.fc_zip_textbox = new System.Windows.Forms.TextBox();
-            this.fc_state_textbox = new System.Windows.Forms.TextBox();
-            this.fc_email_label = new System.Windows.Forms.Label();
-            this.fc_phoneNumber2_label = new System.Windows.Forms.Label();
-            this.fc_lastname_textbox = new System.Windows.Forms.TextBox();
-            this.fc_firstName_textbox = new System.Windows.Forms.TextBox();
-            this.fc_phoneNumber1_label = new System.Windows.Forms.Label();
-            this.fc_zip_label = new System.Windows.Forms.Label();
-            this.fc_state_label = new System.Windows.Forms.Label();
             this.fc_city_label = new System.Windows.Forms.Label();
             this.fc_lastname_label = new System.Windows.Forms.Label();
+            this.fc_state_label = new System.Windows.Forms.Label();
+            this.fc_zip_label = new System.Windows.Forms.Label();
+            this.fc_phoneNumber1_label = new System.Windows.Forms.Label();
+            this.fc_firstName_textbox = new System.Windows.Forms.TextBox();
+            this.fc_lastname_textbox = new System.Windows.Forms.TextBox();
+            this.fc_phoneNumber2_label = new System.Windows.Forms.Label();
+            this.fc_email_label = new System.Windows.Forms.Label();
+            this.fc_state_textbox = new System.Windows.Forms.TextBox();
+            this.fc_zip_textbox = new System.Windows.Forms.TextBox();
+            this.fc_phoneNumber1_textbox = new System.Windows.Forms.TextBox();
+            this.fc_phoneNumber2_textbox = new System.Windows.Forms.TextBox();
+            this.fc_email_textbox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -80,109 +80,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(739, 526);
             this.panel1.TabIndex = 1;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(153, 55);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 16;
-            // 
-            // fc_email_textbox
-            // 
-            this.fc_email_textbox.Location = new System.Drawing.Point(153, 220);
-            this.fc_email_textbox.Name = "fc_email_textbox";
-            this.fc_email_textbox.Size = new System.Drawing.Size(220, 20);
-            this.fc_email_textbox.TabIndex = 15;
-            // 
-            // fc_phoneNumber2_textbox
-            // 
-            this.fc_phoneNumber2_textbox.Location = new System.Drawing.Point(153, 193);
-            this.fc_phoneNumber2_textbox.Name = "fc_phoneNumber2_textbox";
-            this.fc_phoneNumber2_textbox.Size = new System.Drawing.Size(220, 20);
-            this.fc_phoneNumber2_textbox.TabIndex = 14;
-            // 
-            // fc_phoneNumber1_textbox
-            // 
-            this.fc_phoneNumber1_textbox.Location = new System.Drawing.Point(153, 166);
-            this.fc_phoneNumber1_textbox.Name = "fc_phoneNumber1_textbox";
-            this.fc_phoneNumber1_textbox.Size = new System.Drawing.Size(220, 20);
-            this.fc_phoneNumber1_textbox.TabIndex = 13;
-            // 
-            // fc_zip_textbox
-            // 
-            this.fc_zip_textbox.Location = new System.Drawing.Point(153, 98);
-            this.fc_zip_textbox.Name = "fc_zip_textbox";
-            this.fc_zip_textbox.Size = new System.Drawing.Size(100, 20);
-            this.fc_zip_textbox.TabIndex = 12;
-            // 
-            // fc_state_textbox
-            // 
-            this.fc_state_textbox.Location = new System.Drawing.Point(153, 77);
-            this.fc_state_textbox.Name = "fc_state_textbox";
-            this.fc_state_textbox.Size = new System.Drawing.Size(100, 20);
-            this.fc_state_textbox.TabIndex = 11;
-            // 
-            // fc_email_label
-            // 
-            this.fc_email_label.AutoSize = true;
-            this.fc_email_label.Location = new System.Drawing.Point(12, 220);
-            this.fc_email_label.Name = "fc_email_label";
-            this.fc_email_label.Size = new System.Drawing.Size(73, 13);
-            this.fc_email_label.TabIndex = 10;
-            this.fc_email_label.Text = "Email Address";
-            // 
-            // fc_phoneNumber2_label
-            // 
-            this.fc_phoneNumber2_label.AutoSize = true;
-            this.fc_phoneNumber2_label.Location = new System.Drawing.Point(12, 193);
-            this.fc_phoneNumber2_label.Name = "fc_phoneNumber2_label";
-            this.fc_phoneNumber2_label.Size = new System.Drawing.Size(87, 13);
-            this.fc_phoneNumber2_label.TabIndex = 9;
-            this.fc_phoneNumber2_label.Text = "Phone Number 2";
-            // 
-            // fc_lastname_textbox
-            // 
-            this.fc_lastname_textbox.Location = new System.Drawing.Point(153, 32);
-            this.fc_lastname_textbox.Name = "fc_lastname_textbox";
-            this.fc_lastname_textbox.Size = new System.Drawing.Size(220, 20);
-            this.fc_lastname_textbox.TabIndex = 7;
-            // 
-            // fc_firstName_textbox
-            // 
-            this.fc_firstName_textbox.Location = new System.Drawing.Point(153, 10);
-            this.fc_firstName_textbox.Name = "fc_firstName_textbox";
-            this.fc_firstName_textbox.Size = new System.Drawing.Size(220, 20);
-            this.fc_firstName_textbox.TabIndex = 6;
-            // 
-            // fc_phoneNumber1_label
-            // 
-            this.fc_phoneNumber1_label.AutoSize = true;
-            this.fc_phoneNumber1_label.Location = new System.Drawing.Point(12, 166);
-            this.fc_phoneNumber1_label.Name = "fc_phoneNumber1_label";
-            this.fc_phoneNumber1_label.Size = new System.Drawing.Size(87, 13);
-            this.fc_phoneNumber1_label.TabIndex = 5;
-            this.fc_phoneNumber1_label.Text = "Phone Number 1";
-            // 
-            // fc_zip_label
-            // 
-            this.fc_zip_label.AutoSize = true;
-            this.fc_zip_label.Location = new System.Drawing.Point(12, 98);
-            this.fc_zip_label.Name = "fc_zip_label";
-            this.fc_zip_label.Size = new System.Drawing.Size(22, 13);
-            this.fc_zip_label.TabIndex = 4;
-            this.fc_zip_label.Text = "Zip";
-            // 
-            // fc_state_label
-            // 
-            this.fc_state_label.AutoSize = true;
-            this.fc_state_label.Location = new System.Drawing.Point(12, 77);
-            this.fc_state_label.Name = "fc_state_label";
-            this.fc_state_label.Size = new System.Drawing.Size(32, 13);
-            this.fc_state_label.TabIndex = 3;
-            this.fc_state_label.Text = "State";
             // 
             // fc_city_label
             // 
@@ -202,6 +99,108 @@
             this.fc_lastname_label.Size = new System.Drawing.Size(58, 13);
             this.fc_lastname_label.TabIndex = 1;
             this.fc_lastname_label.Text = "Last Name";
+            // 
+            // fc_state_label
+            // 
+            this.fc_state_label.AutoSize = true;
+            this.fc_state_label.Location = new System.Drawing.Point(12, 77);
+            this.fc_state_label.Name = "fc_state_label";
+            this.fc_state_label.Size = new System.Drawing.Size(32, 13);
+            this.fc_state_label.TabIndex = 3;
+            this.fc_state_label.Text = "State";
+            // 
+            // fc_zip_label
+            // 
+            this.fc_zip_label.AutoSize = true;
+            this.fc_zip_label.Location = new System.Drawing.Point(12, 98);
+            this.fc_zip_label.Name = "fc_zip_label";
+            this.fc_zip_label.Size = new System.Drawing.Size(22, 13);
+            this.fc_zip_label.TabIndex = 4;
+            this.fc_zip_label.Text = "Zip";
+            // 
+            // fc_phoneNumber1_label
+            // 
+            this.fc_phoneNumber1_label.AutoSize = true;
+            this.fc_phoneNumber1_label.Location = new System.Drawing.Point(12, 166);
+            this.fc_phoneNumber1_label.Name = "fc_phoneNumber1_label";
+            this.fc_phoneNumber1_label.Size = new System.Drawing.Size(87, 13);
+            this.fc_phoneNumber1_label.TabIndex = 5;
+            this.fc_phoneNumber1_label.Text = "Phone Number 1";
+            // 
+            // fc_firstName_textbox
+            // 
+            this.fc_firstName_textbox.Location = new System.Drawing.Point(153, 10);
+            this.fc_firstName_textbox.Name = "fc_firstName_textbox";
+            this.fc_firstName_textbox.Size = new System.Drawing.Size(220, 20);
+            this.fc_firstName_textbox.TabIndex = 6;
+            // 
+            // fc_lastname_textbox
+            // 
+            this.fc_lastname_textbox.Location = new System.Drawing.Point(153, 32);
+            this.fc_lastname_textbox.Name = "fc_lastname_textbox";
+            this.fc_lastname_textbox.Size = new System.Drawing.Size(220, 20);
+            this.fc_lastname_textbox.TabIndex = 7;
+            // 
+            // fc_phoneNumber2_label
+            // 
+            this.fc_phoneNumber2_label.AutoSize = true;
+            this.fc_phoneNumber2_label.Location = new System.Drawing.Point(12, 193);
+            this.fc_phoneNumber2_label.Name = "fc_phoneNumber2_label";
+            this.fc_phoneNumber2_label.Size = new System.Drawing.Size(87, 13);
+            this.fc_phoneNumber2_label.TabIndex = 9;
+            this.fc_phoneNumber2_label.Text = "Phone Number 2";
+            // 
+            // fc_email_label
+            // 
+            this.fc_email_label.AutoSize = true;
+            this.fc_email_label.Location = new System.Drawing.Point(12, 220);
+            this.fc_email_label.Name = "fc_email_label";
+            this.fc_email_label.Size = new System.Drawing.Size(73, 13);
+            this.fc_email_label.TabIndex = 10;
+            this.fc_email_label.Text = "Email Address";
+            // 
+            // fc_state_textbox
+            // 
+            this.fc_state_textbox.Location = new System.Drawing.Point(153, 77);
+            this.fc_state_textbox.Name = "fc_state_textbox";
+            this.fc_state_textbox.Size = new System.Drawing.Size(100, 20);
+            this.fc_state_textbox.TabIndex = 11;
+            // 
+            // fc_zip_textbox
+            // 
+            this.fc_zip_textbox.Location = new System.Drawing.Point(153, 98);
+            this.fc_zip_textbox.Name = "fc_zip_textbox";
+            this.fc_zip_textbox.Size = new System.Drawing.Size(100, 20);
+            this.fc_zip_textbox.TabIndex = 12;
+            // 
+            // fc_phoneNumber1_textbox
+            // 
+            this.fc_phoneNumber1_textbox.Location = new System.Drawing.Point(153, 166);
+            this.fc_phoneNumber1_textbox.Name = "fc_phoneNumber1_textbox";
+            this.fc_phoneNumber1_textbox.Size = new System.Drawing.Size(220, 20);
+            this.fc_phoneNumber1_textbox.TabIndex = 13;
+            // 
+            // fc_phoneNumber2_textbox
+            // 
+            this.fc_phoneNumber2_textbox.Location = new System.Drawing.Point(153, 193);
+            this.fc_phoneNumber2_textbox.Name = "fc_phoneNumber2_textbox";
+            this.fc_phoneNumber2_textbox.Size = new System.Drawing.Size(220, 20);
+            this.fc_phoneNumber2_textbox.TabIndex = 14;
+            // 
+            // fc_email_textbox
+            // 
+            this.fc_email_textbox.Location = new System.Drawing.Point(153, 220);
+            this.fc_email_textbox.Name = "fc_email_textbox";
+            this.fc_email_textbox.Size = new System.Drawing.Size(220, 20);
+            this.fc_email_textbox.TabIndex = 15;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(153, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 16;
             // 
             // fc_tab
             // 
