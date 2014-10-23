35f114a039e6232173a256c17d06e18886a93139
unknown <kuczmama@CEC-BEN011-6.it.muohio.edu>
2014-10-23 16:01:13 -0400

    added Household Info Form

---
+++ FRCApp/HouseHoldForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FRCApp
+{
+    public partial class HouseHoldForm : UserControl
+    {
+        public HouseHoldForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HouseHoldSubmitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HouseHoldFormLastName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void title_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
