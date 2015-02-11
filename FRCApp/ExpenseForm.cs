58a56149a5ecdc66af71ccb10d14587b32a0c209
RyanDaughters <daughtrc@miamioh.edu>
2015-02-11 11:17:41 -0500

    Added error checking to ExpenseForm

--- FRCApp/ExpenseForm.cs
+++ FRCApp/ExpenseForm.cs
@@ -53,6 +53,14 @@ namespace FRCApp
          **/
         private void submitButton_Click(object sender, EventArgs e)
         {
+            // validate form
+            String message = validateForm();
+            if (message != "OK")
+            {
+                MessageBox.Show(message);
+                return;
+            }
+
             DataSet1TableAdapters.MonthlyExpensesTableAdapter adapter = new DataSet1TableAdapters.MonthlyExpensesTableAdapter();
             adapter.InsertMonthlyExpenses(
                 this.householdID.ToString(), 
@@ -85,6 +93,65 @@ namespace FRCApp
             {
                 this.Close();
             }
+        }
+
+        /**
+         * Validate form, checking for data in each box
+         **/
+        private string validateForm()
+        {
+            if (!textExists(this.ExpenseRentTextBox.Text))
+                return "Enter rent expense.";
+            else if (!textExists(this.ExpenseElectricTextBox.Text))
+                return "Enter electric expense";
+            else if (!textExists(this.ExpenseHeatingTextBox.Text))
+                return "Enter heating expense";
+            else if (!textExists(this.ExpensePhoneTextBox.Text))
+                return "Enter phone expense";
+            else if (!textExists(this.ExpenseTrashTextBox.Text))
+                return "Enter trash expense";
+            else if (!textExists(this.ExpenseWaterTextBox.Text))
+                return "Enter water expense";
+            else if (!textExists(this.ExpensePrescriptionsTextBox.Text))
+                return "Enter prescriptions expense";
+            else if (!textExists(this.ExpenseLaundryTextBox.Text))
+                return "Enter laundry expense";
+            else if (!textExists(this.ExpenseCarPaymentTextBox.Text))
+                return "Enter car payment expense";
+            else if (!textExists(this.ExpenseGasolineTransportTextBox.Text))
+                return "Enter transportation expense";
+            else if (!textExists(this.ExpenseHealthInsuranceTextBox.Text))
+                return "Enter health insurance expense";
+            else if (!textExists(this.ExpenseCableTextBox.Text))
+                return "Enter cable expense";
+            else if (!textExists(this.ExpenseInternetTextBox.Text))
+                return "Enter internet expense";
+            else if (!textExists(this.ExpenseCreditCardTextBox.Text))
+                return "Enter credit card expense";
+            else if (!textExists(this.ExpenseLoansTextBox.Text))
+                return "Enter loan expense";
+            else if (!textExists(this.ExpenseGroceriesTextBox.Text))
+                return "Enter groceries expense";
+            else if (!textExists(this.ExpenseHygieneTextBox.Text))
+                return "Enter hygiene expense";
+            else if (!textExists(this.ExpenseHouseholdTextBox.Text))
+                return "Enter household expense";
+            else if (!textExists(this.ExpenseChildCareTextBox.Text))
+                return "Enter child care expense";
+            else if (!textExists(this.ExpenseChildSupportTextBox.Text))
+                return "Enter child support expense";
+            else if (!textExists(this.ExpenseOtherTextBox.Text))
+                return "Enter other expense";
+            else
+            return "OK";
+        }
+
+        /**
+         * Check if text exists inside a textbox. Used for validating our form
+         **/
+        private bool textExists(string str)
+        {
+            return str != null && str != "";
         }
     }
 }
