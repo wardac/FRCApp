25a88451042e7ca340b4429041e7e6feeb755d22
Andrew Ward <wardac@miamioh.edu>
2015-07-20 18:15:16 -0400

    Added EFA history sorting to db update

--- db_update.sql
+++ db_update.sql
@@ -2,4 +2,28 @@ INSERT INTO dbo.Races
 SELECT 'Multi-racial'
 GO
 
+ALTER PROC dbo.GetInactiveEFARequestsByClientID(@ClientID NVARCHAR(36)) AS BEGIN
+SELECT dis.*
+FROM dbo.EFARequestsDisplay dis
+	JOIN dbo.EFARequests r
+		ON dis.EFARequestID = r.EFARequestID
+	JOIN (SELECT EFARequestID FROM dbo.EFASubrequests WHERE DateClosed IS NOT NULL GROUP BY EFARequestID) sub
+		ON r.EFARequestID = sub.EFARequestID
+WHERE r.ClientID = @ClientID
+ORDER BY dis.DateRequested DESC
+END
+GO
+
+ALTER PROC dbo.GetActiveEFARequestsByClientID(@ClientID NVARCHAR(36)) AS BEGIN
+SELECT dis.*
+FROM dbo.EFARequestsDisplay dis
+	JOIN dbo.EFARequests r
+		ON dis.EFARequestID = r.EFARequestID
+	JOIN (SELECT EFARequestID FROM dbo.EFASubrequests WHERE DateClosed IS NULL GROUP BY EFARequestID) sub
+		ON r.EFARequestID = sub.EFARequestID
+WHERE r.ClientID = @ClientID
+ORDER BY dis.DateRequested DESC
+END
+GO
+
 
